8e3c84c298ac9ca5b05ee852969586a999344f94
Luis Angel Armenta Delgadillo <luigiangelxd@gmail.com>
2022-10-26 20:58:04 -0500

    feat: conversiones para todas las emperaturas implementadas

--- src/model/UnitConverter.java
+++ src/model/UnitConverter.java
@@ -47,6 +47,20 @@ public class UnitConverter {
     }
     
     public double convert() {
+        if ("Temperatura".equals(this.unitTypeSelected)) {
+            switch(this.fromUnit+"-"+this.toUnit) {
+                case "Celsius-Celsius": return this.inputValue;
+                case "Celsius-Kelvin": return this.inputValue + 273.15;
+                case "Celsius-Fahrenheit": return (9 * this.inputValue)/5 + 32;
+                case "Kelvin-Celsius": return this.inputValue - 273.15;
+                case "Kelvin-Kelvin": return this.inputValue;
+                case "Kelvin-Fahrenheit": return ((9 * (this.inputValue - 273.15))/5)  + 32;
+                case "Fahrenheit-Fahrenheit": return this.inputValue;
+                case "Fahrenheit-Celsius": return 5 * (this.inputValue - 32)/9;
+                case "Fahrenheit-Kelvin": return ((5 * (this.inputValue - 32))/9) + 273.15;
+            }
+        }
+        
         double rate = this.conversionRates.get(this.fromUnit + "-" + this.toUnit);
         return this.inputValue * rate;
     }
@@ -83,69 +97,69 @@ public class UnitConverter {
     }
     
     private void loadRates() {
-        this.conversionRates.put("Kilometros-Kilometros", new Double(1));
-        this.conversionRates.put("Kilometros-Metros", new Double(1000));
-        this.conversionRates.put("Kilometros-Centimetros", new Double(1000 * 100));
+        this.conversionRates.put("Kilometros-Kilometros", 1.0);
+        this.conversionRates.put("Kilometros-Metros", 1000.0);
+        this.conversionRates.put("Kilometros-Centimetros", 1000 * 100 + 0.0);
         this.conversionRates.put("Kilometros-Milimetros", 1e+6);
-        this.conversionRates.put("Kilometros-Millas", new Double(1/1.609));
-        this.conversionRates.put("Kilometros-Yardas", new Double(1094));
-        this.conversionRates.put("Kilometros-Pies", new Double(3281));
-        this.conversionRates.put("Kilometros-Pulgadas", new Double(39370));
+        this.conversionRates.put("Kilometros-Millas", 1/1.609);
+        this.conversionRates.put("Kilometros-Yardas", 1094.0);
+        this.conversionRates.put("Kilometros-Pies", 3281.0);
+        this.conversionRates.put("Kilometros-Pulgadas", 39370.0);
 
-        this.conversionRates.put("Metros-Kilometros", new Double(1/1000));
-        this.conversionRates.put("Metros-Metros", new Double(1));
-        this.conversionRates.put("Metros-Centimetros", new Double(100));
-        this.conversionRates.put("Metros-Milimetros", new Double(1000));
-        this.conversionRates.put("Metros-Millas", new Double(1/1609));
-        this.conversionRates.put("Metros-Yardas", new Double(1.094));
-        this.conversionRates.put("Metros-Pies", new Double(3.281));
-        this.conversionRates.put("Metros-Pulgadas", new Double(39.37));
+        this.conversionRates.put("Metros-Kilometros", 0.001);
+        this.conversionRates.put("Metros-Metros", 1.0);
+        this.conversionRates.put("Metros-Centimetros", 100.0);
+        this.conversionRates.put("Metros-Milimetros", 1000.0);
+        this.conversionRates.put("Metros-Millas", 1/1609 + 0.0);
+        this.conversionRates.put("Metros-Yardas", 1.094);
+        this.conversionRates.put("Metros-Pies", 3.281);
+        this.conversionRates.put("Metros-Pulgadas", 39.37);
 
         // Conversion de unidades de superficie
 
-        this.conversionRates.put("Kilometros cuadrados-Kilometros cuadrados", new Double(1));
-        this.conversionRates.put("Kilometros cuadrados-Hectarias", new Double(100));
+        this.conversionRates.put("Kilometros cuadrados-Kilometros cuadrados", 1.0);
+        this.conversionRates.put("Kilometros cuadrados-Hectarias", 100.0);
         this.conversionRates.put("Kilometros cuadrados-Metros cuadrados", 1e+6);
-        this.conversionRates.put("Kilometros cuadrados-Millas cuadradas", new Double(1/2.59));
-        this.conversionRates.put("Kilometros cuadrados-Acres", new Double(247.1));
+        this.conversionRates.put("Kilometros cuadrados-Millas cuadradas", 1/2.59);
+        this.conversionRates.put("Kilometros cuadrados-Acres", 247.1);
         this.conversionRates.put("Kilometros cuadrados-Pies cuadrados", 1.076e+7);
         this.conversionRates.put("Kilometros cuadrados-Pulgadas cuadradas", 1.55e+9);
 
 
         this.conversionRates.put("Metros cuadrados-Kilometros cuadrados", 1/1e+6);
-        this.conversionRates.put("Metros cuadrados-Hectarias", new Double(1/10000));
-        this.conversionRates.put("Metros cuadrados-Metros cuadrados", new Double(1));
-        this.conversionRates.put("Metros cuadrados-Millas cuadradas", new Double(1/2.59e+6));
-        this.conversionRates.put("Metros cuadrados-Acres", new Double(1/4047));
-        this.conversionRates.put("Metros cuadrados-Pies cuadrados", new Double(10.764));
-        this.conversionRates.put("Metros cuadrados-Pulgadas cuadradas", new Double(1550));
+        this.conversionRates.put("Metros cuadrados-Hectarias", 0.0001);
+        this.conversionRates.put("Metros cuadrados-Metros cuadrados", 1.0);
+        this.conversionRates.put("Metros cuadrados-Millas cuadradas", 1/2.59e+6);
+        this.conversionRates.put("Metros cuadrados-Acres", 1/4047 + 0.0);
+        this.conversionRates.put("Metros cuadrados-Pies cuadrados", 10.764);
+        this.conversionRates.put("Metros cuadrados-Pulgadas cuadradas", 1550.0);
 
         // Conversion desde unidades de masa
 
-        this.conversionRates.put("Toneladas-Toneladas", new Double(1));
-        this.conversionRates.put("Toneladas-Kilogramos", new Double(1000));
+        this.conversionRates.put("Toneladas-Toneladas", 1.0);
+        this.conversionRates.put("Toneladas-Kilogramos", 1000.0);
         this.conversionRates.put("Toneladas-Gramos", 1e+6);
         this.conversionRates.put("Toneladas-Toneladas cortas", 1.102);
-        this.conversionRates.put("Toneladas-Libras", new Double(2205));
-        this.conversionRates.put("Toneladas-Onazas", new Double(35270));
+        this.conversionRates.put("Toneladas-Libras", 2205.0);
+        this.conversionRates.put("Toneladas-Onazas", 35270.0);
 
-        this.conversionRates.put("Kilogramos-Toneladas", new Double(1/1000));
-        this.conversionRates.put("Kilogramos-Kilogramos", new Double(1));
-        this.conversionRates.put("Kilogramos-Gramos", new Double(1000));
-        this.conversionRates.put("Kilogramos-Toneladas cortas", new Double(1/907.2));
-        this.conversionRates.put("Kilogramos-Libras", new Double(2.205));
-        this.conversionRates.put("Kilogramos-Onazas", new Double(35.274));
+        this.conversionRates.put("Kilogramos-Toneladas", 0.001);
+        this.conversionRates.put("Kilogramos-Kilogramos", 1.0);
+        this.conversionRates.put("Kilogramos-Gramos", 1000.0);
+        this.conversionRates.put("Kilogramos-Toneladas cortas", 1/907.2);
+        this.conversionRates.put("Kilogramos-Libras", 2.205);
+        this.conversionRates.put("Kilogramos-Onazas", 35.274);
 
         // Conversion de unidades de volumen
         
-        this.conversionRates.put("Metros cubicos-Metros cubicos", new Double(1));
-        this.conversionRates.put("Metros cubicos-Litros", new Double(1000));
+        this.conversionRates.put("Metros cubicos-Metros cubicos", 1.0);
+        this.conversionRates.put("Metros cubicos-Litros", 1000.0);
         this.conversionRates.put("Metros cubicos-Centimetros cubicos", 1e+6);
         this.conversionRates.put("Metros cubicos-Mililitros", 1e+6);
-        this.conversionRates.put("Metros cubicos-Barriles", new Double(6.29));
-        this.conversionRates.put("Metros cubicos-Galones", new Double(264.2));
-        this.conversionRates.put("Metros cubicos-Pintas", new Double(2113));
-        this.conversionRates.put("Metros cubicos-Onzas liquidas", new Double(33810));
+        this.conversionRates.put("Metros cubicos-Barriles", 6.29);
+        this.conversionRates.put("Metros cubicos-Galones", 264.2);
+        this.conversionRates.put("Metros cubicos-Pintas", 2113.0);
+        this.conversionRates.put("Metros cubicos-Onzas liquidas", 33810.0);
         
         this.conversionRates.put("Litros-Metros cubicos", 0.001);
         this.conversionRates.put("Litros-Litros", 1.0);
