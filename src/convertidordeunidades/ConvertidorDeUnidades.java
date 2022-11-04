abfe8f71280f17e6f2c09e76b1a8da26965f6ae0
Luis Angel Armenta Delgadillo <luigiangelxd@gmail.com>
2022-11-04 12:18:44 -0600

    feat: portada del equipo agregada

--- src/convertidordeunidades/ConvertidorDeUnidades.java
+++ src/convertidordeunidades/ConvertidorDeUnidades.java
@@ -3,16 +3,32 @@ package convertidordeunidades;
 import views.Vista;
 import model.UnitConverter;
 import controllers.Controller;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import views.Front;
 
 public class ConvertidorDeUnidades {
     public static void main(String[] args) {
+        Front titlePage = new Front();
+        
+        titlePage.setVisible(true);
+        titlePage.setTitle("Portada / Convertidor de unidades / Equipo 3");
+        titlePage.setLocationRelativeTo(null);
+        
         Vista view = new Vista();
         UnitConverter model = new UnitConverter();
         Controller controller = new Controller(view, model);
         
         view.setTitle("CONVERTIDOR DE UNIDADES");
         view.setLocationRelativeTo(null);
-        view.setVisible(true);
+        
+        titlePage.jButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                titlePage.setVisible(false);
+                view.setVisible(true);
+            }
+        });
     }
     
 }
